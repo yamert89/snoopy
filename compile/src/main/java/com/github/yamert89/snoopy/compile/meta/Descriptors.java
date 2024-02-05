6556babaae062d30dd408c7debd86f71fa0b5f34
Alexander Porokhin <softoad@yandex.ru>
2024-02-05 21:05:26 +0300

    compile: add com.github to Descriptors

--- compile/src/main/java/com/github/yamert89/snoopy/compile/meta/Descriptors.java
+++ compile/src/main/java/com/github/yamert89/snoopy/compile/meta/Descriptors.java
@@ -1,8 +1,8 @@
 package com.github.yamert89.snoopy.compile.meta;
 
 public class Descriptors {
-    public static final String REPLACE_SQL = "Lyamert89/snoopy/meta/InjectSQL;";
-    public static final String REPLACE_SQL_FIELD = "Lyamert89/snoopy/meta/InjectSQLField;";
+    public static final String REPLACE_SQL = "Lcom/github/yamert89/snoopy/meta/InjectSQL;";
+    public static final String REPLACE_SQL_FIELD = "Lcom/github/yamert89/snoopy/meta/InjectSQLField;";
     public static final String STRING = "Ljava/lang/String;";
     public static final String INIT = "<init>";
     public static final String FIELD_RESOURCE_NAME = "name";
