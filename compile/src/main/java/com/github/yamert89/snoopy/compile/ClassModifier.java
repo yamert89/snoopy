0e0d44fbd230a7b6ff6875a09e879af27561c501
Alexander Porokhin <softoad@yandex.ru>
2024-02-11 20:28:15 +0300

    compile: add documentation to ClassModifier

--- compile/src/main/java/com/github/yamert89/snoopy/compile/ClassModifier.java
+++ compile/src/main/java/com/github/yamert89/snoopy/compile/ClassModifier.java
@@ -3,6 +3,14 @@ package com.github.yamert89.snoopy.compile;
 import java.io.File;
 import java.util.List;
 
+/**
+ * Here is the entry point to the public API
+ */
 public interface ClassModifier {
+
+    /**
+     * Receives the collection of class files and the collection of .sql scripts,
+     * modifies the classes
+     */
     void modify(List<File> classes, List<File> resources);
 }
